39205e160e585406db739c1fb1be57a55d88db66
Yifang-Guo <charlizeguo@gmail.com>
2025-07-16 17:25:36 +0100

    fix workspace display bugs

--- curationTool/reactions/static/reactions/js_VMHWorkspace/workspace.js
+++ curationTool/reactions/static/reactions/js_VMHWorkspace/workspace.js
@@ -58,7 +58,8 @@ document.querySelectorAll('.subtabs').forEach((bar) => {
 		const card = await buildEditableCard(rxn); // ← await it
 		if (card) detailEl.appendChild(card);
 
-		attachDynamicEventListeners();
+		document.querySelectorAll('#wsReactionList .item').forEach((el) => el.classList.remove('active'));
+		e.target.classList.add('active');
 	});
 
 	// 3 – helper that re-uses your modal-builders
@@ -132,7 +133,7 @@ document.querySelectorAll('.subtabs').forEach((bar) => {
                     </div>
 
                     <div class="reaction-field">
-                        <label>Abbreviation:</label>
+                        <label>Reaction Abbreviation:</label>
                         <input type="text" class="reaction-abbreviation-input" placeholder="Enter reaction abbreviation"
                             value="${reaction.fields.short_name}" data-reaction-id="${reaction.pk}">
                     </div>
@@ -299,3 +300,32 @@ document.querySelectorAll('.subtabs').forEach((bar) => {
 		addToVMH(); // comes from handleAdd2VMH.js
 	}
 })();
+
+const modalList = document.getElementById('wsReactionDetails');
+
+modalList.addEventListener('click', function (e) {
+	// Handle the "Add" button clicks using event delegation
+	if (e.target && e.target.matches('.add-reference, .add-ext-link, .add-comment')) {
+		const type = e.target.classList.contains('add-reference') ? 'reference' : e.target.classList.contains('add-ext-link') ? 'ext-link' : 'comment';
+		const parentSection = e.target.parentNode;
+		const reactionId = e.target.getAttribute('data-reaction-id');
+		const newItem = document.createElement('div');
+		newItem.className = `${type}-item`;
+		const items = parentSection.querySelectorAll(`.${type}-item`);
+		const newIndex = items.length; // Calculate new index based on existing items
+		newItem.setAttribute('data-reaction-id', reactionId);
+		newItem.setAttribute('data-index', newIndex);
+		newItem.innerHTML = type === 'ext-link' ? createExtLinkSelect({}, reactionId, newIndex) : '';
+		newItem.innerHTML += type === 'reference' ? createRefSelect({}, reactionId, newIndex) : '';
+		newItem.innerHTML += `
+            <input type="text" class="${type}-input" placeholder="Enter ${type}" value="" data-reaction-id="${reactionId}" data-index="${newIndex}">
+            <button class="remove-${type}" data-reaction-id="${reactionId}" data-index="${newIndex}">Remove</button>
+        `;
+		parentSection.insertBefore(newItem, e.target);
+	}
+
+	// Handle the "Remove" button clicks using event delegation
+	if (e.target && e.target.matches('.remove-reference, .remove-ext-link, .remove-comment,.remove-gene-info')) {
+		e.target.parentElement.remove();
+	}
+});
