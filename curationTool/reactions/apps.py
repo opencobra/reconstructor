21f2bd4e9623657349a2efc6670e490d53685073
Yifang-Guo <charlizeguo@gmail.com>
2025-07-23 11:26:26 +0100

    fix: keep Matlab session alive

--- curationTool/reactions/apps.py
+++ curationTool/reactions/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from reactions.utils.MatlabSessionManager import MatlabSessionManager
 
 
 class ReactionsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "reactions"
+    
+    def ready(self):
+        # Start MATLAB session once at startup
+        MatlabSessionManager()
